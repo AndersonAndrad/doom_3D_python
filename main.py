93706ad6b2c208f595cd2c0e2f009d4301251b3e
Anderson Andrade <anderson_andrade_@outlook.com>
2024-01-01 12:32:59 -0300

    feat: implements player from the game

--- main.py
+++ main.py
@@ -4,25 +4,30 @@ import pygame as pg
 import sys
 from settings import *
 from map import *
+from player import *
 
 class Game:
     def __init__(self) -> None:
         pg.init()
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
+        self.delta_time = 1
         self.new_games()
         
     def new_games(self):
         self.map = Map(self)
+        self.player = Player(self)
         
     def update(self):
         pg.display.flip()
-        self.clock.tick(FPS)
+        self.player.update()
+        self.delta_time = self.clock.tick(FPS)
         pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
         
     def draw(self):
         self.screen.fill('black')
         self.map.draw()
+        self.player.draw()
         
     def check_events(self):
         for event in pg.event.get():
